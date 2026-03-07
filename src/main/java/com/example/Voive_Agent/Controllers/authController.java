1eafba8d314aea9ff5312c62511c072135691749
suryansh srivastava <iamsuryansh2@gmail.com>
2026-03-08 01:59:14 +0530

    first commit

---
+++ src/main/java/com/example/Voive_Agent/Controllers/authController.java
@@ -0,0 +1,34 @@
+package com.example.Voive_Agent.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class authController {
+    public Integer authkey;
+    private final Object lock = new Object();
+
+    @GetMapping
+    public String auth() throws InterruptedException {
+
+        synchronized (lock) {
+
+            if (authkey == null || authkey == 0) {
+                lock.wait(15000); // wait max 15 sec
+            }
+
+            if (authkey != null && authkey != 0) {
+                return "Key: " + authkey;
+            } else {
+                return "Auth key not set within 15 seconds";
+            }
+        }
+    }
+    @PostMapping("/setauthkey/{key}")
+    public String setAuthKey(@PathVariable Integer key) {
+        this.authkey = key;
+        return "Auth key set to: " + authkey;
+    }
+}
+
---
+++ src/main/java/com/example/Voive_Agent/VoiveAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Voive_Agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiveAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiveAgentApplication.class, args);
+	}
+
+}
